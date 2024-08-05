4a8b7fce94d3f92e2ffca913eef5c0351c88ac5f
Pasha Malik <pashamalik9371@gmail.com>
2024-08-05 22:50:34 +0700

    add null re session

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,47 +1,50 @@
-const express = require('express');
-const path = require('path');
-const adminController = require('../controllers/adminController');
+const express = require("express");
+const path = require("path");
+const adminController = require("../controllers/adminController");
 
 const router = express.Router();
 
 
-
-// Endpoint untuk mengirimkan data user
-router.get('/data/user/json', (req, res) => {
-    if (req.session.user && req.session.user.role === 'admin') {
+// Endpoint for sending user data
+router.get("/data/user/json", (req, res) => {
+    if (req.session.user) {
       res.json({
-        role: req.session.user.role,
+        id: req.session.user.id,
         username: req.session.user.username,
+        role: req.session.user.role,
       });
     } else {
-      res.sendStatus(401); // Unauthorized jika user bukan admin
+      res.json({
+        id: null,
+        username: "visitor",
+        role: "visitor",
+      });
     }
   });
 
 // Admin Dashboard
 router.get("/dashboard", (req, res) => {
-    if (req.session.user && req.session.user.role === "admin") {
-      res.sendFile(path.join(__dirname, "../../public/admin", "dashboard.html"));
-    } else {
-      res.redirect("/login");
-    }
-  });
-  
-  router.get("/forms", (req, res) => {
-    if (req.session.user && req.session.user.role === "admin") {
-      res.sendFile(path.join(__dirname, "../../public/admin", "forms.html"));
-    } else {
-      res.redirect("/login");
-    }
-  });
-  
-  router.get("/tables", (req, res) => {
-    if (req.session.user && req.session.user.role === "admin") {
-      res.sendFile(path.join(__dirname, "../../public/admin", "tables.html"));
-    } else {
-      res.redirect("/login");
-    }
-  });
-  
-  module.exports = router;
-  
+  if (req.session.user && req.session.user.role === "admin") {
+    res.sendFile(path.join(__dirname, "../../public/admin", "dashboard.html"));
+  } else {
+    res.redirect("/login");
+  }
+});
+
+router.get("/forms", (req, res) => {
+  if (req.session.user && req.session.user.role === "admin") {
+    res.sendFile(path.join(__dirname, "../../public/admin", "forms.html"));
+  } else {
+    res.redirect("/login");
+  }
+});
+
+router.get("/tables", (req, res) => {
+  if (req.session.user && req.session.user.role === "admin") {
+    res.sendFile(path.join(__dirname, "../../public/admin", "tables.html"));
+  } else {
+    res.redirect("/login");
+  }
+});
+
+module.exports = router;
