76027ea766483b4f82f030898c851e4075887db3
Pasha Malik <pashamalik9371@gmail.com>
2024-08-05 23:57:13 +0700

    state poduct

--- src/app.js
+++ src/app.js
@@ -6,23 +6,32 @@ const session = require("express-session");
 const express = require('express');
 const path = require('path');
 const cors = require("cors")
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
-
 
 const app = express();
 
 
 app.use(cors())
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-      maxAge: 60 * 60 * 1000 // 1 jam dalam milidetik
-    }
-  })
-);
+// Middleware for parsing cookies
+app.use(cookieParser()); // Cookie secret for signed cookies
+
+// Middleware for parsing request bodies
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Session middleware
+app.use(session({
+  secret: process.env.SESSION_SECRET, // Secret used to sign the session ID cookie
+  resave: false, // Do not save session if unmodified
+  saveUninitialized: false, // Do not create a session until something is stored
+  cookie: {
+    maxAge: 60 * 60 * 1000, // 1 hour in milliseconds
+    secure: 'production', // Set to true if using HTTPS
+    sameSite: 'lax' // Controls when cookies are sent
+  }
+}));
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
